6358f7a117fb03feca14c2a4d654085cd5babc55
Sumanbitw <007sumanghosh@gmail.com>
2025-02-02 20:44:28 +0530

    add: solution of all the hard problems

--- week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,43 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const startTime = new Date().getTime();
+  return wait1(t1)
+    .then(() => {
+      return wait2(t2);
+    })
+    .then(() => {
+      return wait3(t3);
+    })
+    .then(() => {
+      const totalTime = new Date().getTime() - startTime;
+      console.log(totalTime);
+      return totalTime;
+    });
 }
 
 module.exports = calculateTime;
